c8643c378f992bae080db5eee3ab2da86a611679
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-09-20 10:21:44 -0600

    working on testing server

--- auth/auth.spec.js
+++ auth/auth.spec.js
@@ -5,5 +5,14 @@ describe('auth-model', () => {
     beforeEach(async () => {
         await db('shoes').truncate()
     })
-    
+
+    describe('add()', () => {
+        it('should add user to db', async () => {
+            await logi.add({ username: 'net', password: 'net'})
+
+            let user = await db('users')
+
+            expect(user).toHaveLength(2)
+        })
+    })
 })
